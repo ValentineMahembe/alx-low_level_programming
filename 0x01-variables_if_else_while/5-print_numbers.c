d40b4255e931f30317b89043292e41f90f2febda
Valentine Mahembe <mahembevalentine@gmail.com>
2023-03-20 03:57:49 -0700

    5. Print numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Description: print numbers 1 to 10
+ * Return: 0 (succes)
+ */
+int main(void)
+{
+	int a;
+
+	for (a = 0; a <= 9; a++)
+		printf("%d" ,a);
+	printf("\n");
+	return (0);
+}
