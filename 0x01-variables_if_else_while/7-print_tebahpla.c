0d64951b22ecb0af690105f164b93bd275a4fca8
Valentine Mahembe <mahembevalentine@gmail.com>
2023-03-20 05:27:46 -0700

    7. Smile in the mirror

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Description: print number 0 to 9
+ * Return: 0 (success)
+ */
+int main(void)
+{
+	char c;
+	
+	for (c = 'z'; c >= 'a'; --c)
+		putchar(c);
+	putchar('\n');
+	return (0);
+}
