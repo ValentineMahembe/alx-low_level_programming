a027f3d0bf200e6a4f96c0e7d4c55832d77ed5d3
Valentine Mahembe <mahembevalentine@gmail.com>
2023-03-22 10:35:07 -0700

    0. Multiple mains

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * main - Tests a function that prints if integer is pos or neg
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	int i;
+
+	i = 0;
+	positive_or_negative(i);
+
+	return (0);
+}
