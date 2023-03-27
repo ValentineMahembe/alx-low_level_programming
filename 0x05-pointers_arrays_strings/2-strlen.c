9a2d39ced96e71517b57f18fd7c025d3f673580c
Valentine Mahembe <mahembevalentine@gmail.com>
2023-03-27 10:52:32 -0700

    2. String lenght

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _strlen - returns the lenght of a string
+ * @*s: string lenght to be returned
+ */
+int _strlen(char *s)
+{
+	int len = 0;
+
+	while (*s != '\0')
+	{
+		len++;
+		s++;
+	}
+	return len;
+}
