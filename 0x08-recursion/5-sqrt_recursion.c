521d24c400fa0809ef28ad16ffb4630738f39554
Valentine Mahembe <mahembevalentine@gmail.com>
2023-04-05 10:31:50 -0700

    a function that returns the natural square root of a number 3

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -13,32 +13,5 @@ int _sqrt_recursion(int n)
 		return (-1);
 	else if (n == 0 || n == 1)
 		return (n);
-	else return (_sqrt_recursion_helper(n, 1, n));
-}
-
-/**
- * _sqrt_recursion_helper - returns the square root on n
- *
- * @mid: mid point of the range between 1 and n
- * @n: number to be processed
- *
- * Return: square root of n
- */
-int _sqrt_recursion_helper(int n, int start, int end)
-{
-	int mid;
-
-	if (end >= start)
-	{
-		mid = (start + end)  / 2;
-
-		if (mid * mid == n)
-			return (mid);
-
-		if (mid * mid > n)
-			return (_sqrt_recursion_helper(n, start, mid - 1));
-		return (_sqrt_recursion_helper(n, mid + 1, end));
-	}
-
-	return (-1);
+	else return (_sqrt_recursion(n / 2) * 2 == n ? _sqrt_recursion(n / 2) : -1);
 }
