2d98ec93a7df2d31a6e0ff11309428450edec87d
Valentine Mahembe <mahembevalentine@gmail.com>
2023-04-11 02:51:03 -0700

    A static library libmy.a containing all the functions

---
+++ 0x09-static_libraries/0-isupper.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * _isupper - check if its upper case
+ *
+ * @c: is an integer parameter
+ * Return: Always 0.
+ */
+int _isupper(int c)
+{
+	return (c >= 65 && c <= 90);
+}
