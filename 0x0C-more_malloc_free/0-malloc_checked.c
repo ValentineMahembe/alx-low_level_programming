5342a498f0e7d3fdb9d43c4ed8470fdfb4f0dd8c
Valentine Mahembe <mahembevalentine@gmail.com>
2023-06-16 12:51:50 -0700

    Malloc 1 and 2

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -1,18 +1,17 @@
 #include "main.h"
-#include <stdlib.h>
 
 /**
- * malloc_checked - allocates memory using malloc
- * @b: number if bytes
+ * malloc_checked - allocates memory using malloc, exit(98) if it fails
+ * @b: size of the memory block to be allocated
  *
- * Return: pointer to the allocatedmemory
+ * Return: pointer to the address of the memory block
  */
 void *malloc_checked(unsigned int b)
 {
-	void *p;
-	
-	p = malloc(b);
-	if (p == NULL)
+	void *block;
+
+	block = malloc(b);
+	if (block == NULL)
 		exit(98);
-	return (p);
+	return (block);
 }
