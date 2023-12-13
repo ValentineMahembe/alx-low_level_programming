0c45cb130091c5a78197720430ca86c331f78677
Valentine Mahembe <mahembevalentine@gmail.com>
2023-12-13 12:51:15 +0200

    1. Binary Search

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -5,5 +5,9 @@
 #include <stdlib.h>
 
 int linear_search(int *array, size_t size, int value);
+int binary_search(int *array, size_t size, int value);
+
+/* HELPER FUNCTIONS */
+void print_array(int *array, size_t size);
 
 #endif /* SEARCH_ALGOS_H */
